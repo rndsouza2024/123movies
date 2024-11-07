c7e74d51922c2ef956d901bce7a6a8c429934020
mxplayermovies <dsouzarnd@gmail.com>
2024-11-07 09:33:15 +0530

    ass hole

--- components/VideoPlayerAds1.js
+++ components/VideoPlayerAds1.js
@@ -182,7 +182,6 @@
 
 
 
-
 import React, { useEffect, useRef, useState } from "react";
 import youtubeStyles from "@styles/MovieDetail.module.css"; // Import CSS for the MovieDetail component
 
@@ -210,7 +209,7 @@ const VideoPlayerWithAds1 = ({ adTagUrl, src }) => {
   };
 
   const playAd = (adUrl) => {
-    adOverlayRef.current.style.display = "flex";
+    adOverlayRef.current.classList.add(youtubeStyles.active); // Show the ad overlay
     const adVideo = document.createElement("video");
     adVideo.src = adUrl;
     adVideo.controls = true;
@@ -219,7 +218,7 @@ const VideoPlayerWithAds1 = ({ adTagUrl, src }) => {
     adVideo.style.position = "absolute";
     adVideo.style.zIndex = "1";
     adVideo.onended = () => {
-      adOverlayRef.current.style.display = "none";
+      adOverlayRef.current.classList.remove(youtubeStyles.active); // Hide the ad overlay when ad ends
       setIsAdPlaying(false);
       adVideo.remove();
     };
@@ -251,7 +250,7 @@ const VideoPlayerWithAds1 = ({ adTagUrl, src }) => {
   const handleSkipAd = () => {
     setIsAdPlaying(false);
     if (adOverlayRef.current) {
-      adOverlayRef.current.style.display = "none";
+      adOverlayRef.current.classList.remove(youtubeStyles.active); // Hide the overlay when skipping
       adOverlayRef.current.innerHTML = "";
     }
   };
@@ -265,20 +264,7 @@ const VideoPlayerWithAds1 = ({ adTagUrl, src }) => {
       {isAdPlaying && (
         <div
           ref={adOverlayRef}
-          className={youtubeStyles.adOverlay}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "white",
-            marginTop: "300px",
-            zIndex: 2,
-          }}
+          className={`${youtubeStyles.adOverlay} ${showSkipButton ? "active" : ""}`}
         >
           <div style={{ textAlign: "center", zIndex: 3 }}>
             {!showSkipButton ? (
@@ -298,7 +284,7 @@ const VideoPlayerWithAds1 = ({ adTagUrl, src }) => {
                   fontWeight: "bold",
                   fontSize: "20px",
                   padding: "8px 16px",
-                  marginTop: "300px",
+                  marginTop: "10px",
                   color: "white",
                   border: "none",
                   borderRadius: "5px",
@@ -311,30 +297,10 @@ const VideoPlayerWithAds1 = ({ adTagUrl, src }) => {
                 Skip Ad
               </button>
             )}
-            <a
-              href="https://www.amazon.in/JBL-130NC-Active-Cancellation-Earbuds/dp/B09HGSD4TD?crid=GWDPS7LEBLA9&dib=eyJ2IjoiMSJ9.M6O91hr-Aeegum3J8TgcDgF-GS4ug9Qxzn-RolTSIv3GtakQ8r-lZd6Vam0cF4tPv__OHZbWTVeblSfDQFp9xA270oWGNW39Zi8r00Va4Jk3dYktXoq_YLpr5FjY-BfdGt-jJK_SzrqCIRmR1lyAI4s1BA3151kqmTi_vzFIp3iEXfFxTW-QPDuriUMY3U3LhiWbTSIl50J3eS8xTnIIkV44lJ2Qnus0JlqSm62SgmzrftyoEe1UD91ykqJBtFGTIfzrWV2PSX4P2eJExqtPaxGyvDdC9V7yg4g9ZrMYjD4.H5GALnNJEKZtKRh-S29gz2nSyu_4D0KsL8L4Vsu_XFE&dib_tag=se&keywords=headphones%2BBUDS%2Bnoise%2Bcancellation&qid=1730945886&refinements=p_123%3A233043&rnid=91049095031&s=electronics&sprefix=headphones%2Bbuds%2Bnoise%2Bcancellation%2Celectronics%2C194&sr=1-1&th=1&linkCode=ll1&tag=rnd08c-21&linkId=4139e3dedaea1c1d00bac7faed0cf9f3&language=en_IN&ref_=as_li_ss_tl"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{
-                position: "relative",
-                padding: "8px 16px",
-                backgroundColor: "rgba(0, 0, 0, 0.6)",
-                color: "white",
-                borderRadius: "5px",
-                fontWeight: "bold",
-                fontSize: "20px",
-                textDecoration: "none",
-                zIndex: 3,
-                marginTop: "10px", // Added margin for spacing below "Skip Ad"
-              }}
-            >
-              Click Now 2 BUY !!!
-            </a>
           </div>
         </div>
       )}
 
-      {/* Video Player */}
       {!isAdPlaying && src && (
         <iframe
           ref={playerRef}
@@ -346,7 +312,7 @@ const VideoPlayerWithAds1 = ({ adTagUrl, src }) => {
           allow="fullscreen; picture-in-picture"
           webkitAllowFullScreen={true}
           mozAllowFullScreen={true}
-          style={{ borderRadius: "15px", marginTop: "300px", zIndex: 1 }}
+          style={{ borderRadius: "15px", zIndex: 1 }}
         />
       )}
     </div>
